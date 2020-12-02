6a0682e91130972651f3c2afd9ce297306b7a18c
Igor Lukanin <me@igorlukanin.io>
2020-12-02 12:09:26 +0300

    igorlukanin: Day 2

---
+++ igorlukanin/2-1.kts
@@ -0,0 +1,28 @@
+// https://adventofcode.com/2020/day/2
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+data class Entry(
+        val lower: Int,
+        val upper: Int,
+        val letter: Char,
+        val password: String
+) {
+    val isValid: Boolean
+        get() {
+            val count = password.count { it == letter }
+            return count in lower..upper
+        }
+}
+
+val entries = Files
+        .readAllLines(Paths.get(".inputs/2.txt"))
+        .map {
+            val parts = it.split('-', ' ', ':')
+            Entry(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), parts[2][0], parts[4])
+        }
+
+val valid = entries.filter { it.isValid }
+
+print(valid.size)
---
+++ igorlukanin/2-2.kts
@@ -0,0 +1,29 @@
+// https://adventofcode.com/2020/day/2
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+data class Entry(
+        val lower: Int,
+        val upper: Int,
+        val letter: Char,
+        val password: String
+) {
+    val isValid: Boolean
+        get() {
+            val first = password[lower - 1]
+            val last = password[upper - 1]
+            return (first == letter || last == letter) && !(first == letter && last == letter)
+        }
+}
+
+val entries = Files
+        .readAllLines(Paths.get(".inputs/2.txt"))
+        .map {
+            val parts = it.split('-', ' ', ':')
+            Entry(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), parts[2][0], parts[4])
+        }
+
+val valid = entries.filter { it.isValid }
+
+print(valid.size)
